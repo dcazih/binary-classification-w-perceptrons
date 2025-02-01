e69180aaa528776f94fca2b7ea3447f7452f797c
Dmitri Azih <dmitriazih@gmail.com>
2025-02-01 15:14:20 -0700

    finished update to image_data_format_check, for proper binary image data validation

--- src/ProcessImageData.py
+++ src/ProcessImageData.py
@@ -36,6 +36,26 @@ class ProcessBinaryImgData:
 
         """
 
+        # Create directory path and its subdirectories
+        img_data_path = Path(img_dir)
+        subdirs = [d for d in img_data_path.iterdir() if d.is_dir()]  # finds the given path's subdirectories
+
+        # Ensure subdirectory requirement
+        if subdirs != 2:
+            raise ValueError(f"Error: Expected exactly two subdirectories, but found {len(subdirs)}.")
+
+        accepted_img_types = {'.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff'}
+
+        # A function to check if a directory contains any images
+        def contains_image(directory):
+            return any((f.suffix_lower() in accepted_img_types) for f in directory.iterdir() if f.is_file())
+
+        # Ensure image subdirectory requirement
+        if not all(contains_image(sd) for sd in subdirs):
+            raise ValueError("Error: Both subdirectories must contain at least one image file.")
+
+        # Return validated image subdirectories
+        return subdirs[0], subdirs[1]
 
     def preprocessing(self, img_dir):
         """
@@ -54,7 +74,3 @@ class ProcessBinaryImgData:
             y - Class labels for images where 1 - first image folder (cat), 0 - second image folder(dog)
             Both arrays randomly permutated together
         """
-
-
-
-
