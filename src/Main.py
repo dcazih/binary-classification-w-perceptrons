5261e88188c1d7e3e9c52068470bde00dee92c7f
Dmitri Azih <dmitriazih@gmail.com>
2025-02-02 16:59:16 -0700

    Main either processes data or loads previous data

--- src/Main.py
+++ src/Main.py
@@ -1,5 +1,22 @@
-from data_processing.bin_img_processing import preprocess
+import numpy as np
+from data_processing.BinImgProcessor import BinImgProcessor
+from src.Perceptron import Perceptron
 
 image_data_path = r"C:\Users\azihd\OneDrive\Documents\binary-classification-w-perceptrons\PetImages"
+handler = BinImgProcessor(image_data_path)
 
-X, y = preprocess(image_data_path)
+if handler.processed_data_path.exists():
+    print("Loading previous data ... ")
+    X, y = handler.load()
+    print("Done!\n")
+else:
+    print("Processing image data ... ")
+    handler.process(image_data_path)
+    print("Loading data ... ")
+    X, y = handler.load()
+    print("Done!\n")
+
+
+
+
+
