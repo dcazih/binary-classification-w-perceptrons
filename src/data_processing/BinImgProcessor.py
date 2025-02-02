f5b49675616fe1bc29df93b51eae80622ff83199
Dmitri Azih <dmitriazih@gmail.com>
2025-02-02 16:58:21 -0700

    added load method to BinImgProcessor.py class, processed_data_path now contains full path

--- src/data_processing/BinImgProcessor.py
+++ src/data_processing/BinImgProcessor.py
@@ -1,4 +1,4 @@
-from bin_img_processing_utils import preprocess
+from data_processing.bin_img_processing_utils import preprocess
 from pathlib import Path
 import numpy as np
 
@@ -6,17 +6,15 @@ class BinImgProcessor:
 
     def __init__(self, img_dir):
         self.img_dir = img_dir
-        self.processed_data_path = Path(__file__).parent / "processed_data"
+        self.processed_data_path = Path(__file__).parent / "processed_data" / "processed_images.npz"
+
+    def process(self, img_dir):
         self.X, self.y = preprocess(img_dir)  # process data
+        np.savez_compressed(self.processed_data_path, X=self.X, y=self.y)  # store data
+        print(f"Data saved to {self.processed_data_path}")
 
-        np.savez_compressed("processed_images.npz", X=self.X, y=self.y)  # store processed data
-        print(f"Data saved to {self.processed_data_path / 'processed_images.npz'}")
-
-    def reprocess(self, img_dir):
-        self.X, self.y = preprocess(img_dir)  # process data
-        np.savez_compressed(self.processed_data_path / "processed_images.npz", X=self.X, y=self.y)  # store data
-        print(f"Data saved to {self.processed_data_path / 'processed_images.npz'}")
-
-
-
-
+    def load(self):
+        image_data = np.load(self.processed_data_path)
+        X, y = image_data['X'], image_data['y']
+        image_data.close()
+        return X, y
