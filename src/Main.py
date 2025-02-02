c2bc693012fe273e21cbfaf107ef781b02b5dd75
Dmitri Azih <dmitriazih@gmail.com>
2025-02-02 11:06:37 -0700

    Testing package imports and function calls in Main

--- src/Main.py
+++ src/Main.py
@@ -1,2 +1,5 @@
-import numpy as np
+from data_processing.bin_img_processing import preprocess
 
+image_data_path = r"C:\Users\azihd\OneDrive\Documents\binary-classification-w-perceptrons\PetImages"
+
+X, y = preprocess(image_data_path)
