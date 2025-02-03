a935a5a8060d432b330652e316e374fe6a81cc8e
Dmitri Azih <dmitriazih@gmail.com>
2025-02-02 17:01:47 -0700

    added train() method to fit model based on training data X, y

--- src/Perceptron.py
+++ src/Perceptron.py
@@ -1,26 +1,26 @@
 import numpy as np
 class Perceptron:
-    """
-    Perceptron Classifiers
+    """Perceptron Classifiers
 
-        Parameters
-        __________
-        eta : float
-            Learning rate (between 0.0 and 1.0)
-        n_epochs : int
-            Num of passes over the training dataset
-        random_seed : int
-            Random number generator seed for random weight initialization
+    Parameters
+    __________
+    eta : float
+        Learning rate (between 0.0 and 1.0)
+    n_epochs : int
+        Num of passes over the training dataset
+    random_seed : int
+        Random number generator seed for random weight initialization
 
-        Attributes
-        __________
+    Attributes
+    __________
 
-        w_ : 1d_array
-            Final weights after training
-        b_ : Scalar
-            Final bias after training
-        errors_ : list
-            Number of classifications in each epoch
+    w_ : 1d_array
+        Final weights after training
+    b_ : Scalar
+        Final bias after training
+    errors_ : list
+        Number of classifications in each epoch
+
     """
 
     def __init__(self, eta=0.01, n_epochs=50, random_seed=4078):
@@ -45,4 +45,22 @@ class Perceptron:
         self : object
             Returns the instance itself."""
 
+        # Initialize a random normal dist of weights
+        rgen = np.random.RandomState(self.random_seed)
 
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=X.shape[1])
+
+        # Initialize bias and errors
+        self.b_ = np.float_(0.0)
+        self.errors_ = []
+
+        # Run training
+        for i in range(self.n_epochs):
+            errors = 0
+            for xi, target in zip (X, y):
+                update = self.eta * (target - self.predict(xi))  # update = η * (yi − ̂yi)
+                self.w_ += update * xi  # w_ = w_ + update * xi
+                self.b_ += update  # b_ = b_ + update
+                errors += int(update != 0.0)  # Check prediction: 1 wrong, 0 correct
+            self.errors_.append(errors)
+        return self
