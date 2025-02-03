b697d956c1b5c59de6edde08a6408ad0f5825410
Dmitri Azih <dmitriazih@gmail.com>
2025-02-02 17:36:51 -0700

    Data is now split into a training set and test set

--- src/data_processing/BinImgProcessor.py
+++ src/data_processing/BinImgProcessor.py
@@ -1,4 +1,5 @@
 from data_processing.bin_img_processing_utils import preprocess
+from sklearn.model_selection import train_test_split
 from pathlib import Path
 import numpy as np
 
@@ -9,12 +10,20 @@ class BinImgProcessor:
         self.processed_data_path = Path(__file__).parent / "processed_data" / "processed_images.npz"
 
     def process(self, img_dir):
-        self.X, self.y = preprocess(img_dir)  # process data
-        np.savez_compressed(self.processed_data_path, X=self.X, y=self.y)  # store data
+        # Obtain image data
+        self.X, self.y = preprocess(img_dir)
+
+        # Split the data into training and testing (90% for training, 10% for testing)
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.1,
+                                                                                random_state=42)
+        # Compress and store data
+        np.savez_compressed(self.processed_data_path, X=self.X_train, y=self.y_train, X_test=self.X_test,
+                                                                                y_test=self.y_test)
         print(f"Data saved to {self.processed_data_path}")
 
     def load(self):
         image_data = np.load(self.processed_data_path)
-        X, y = image_data['X'], image_data['y']
+        self.X_train, self.X_test, self.y_train, self.y_test = image_data['X'], image_data['y'], image_data['X_test'],\
+                                                                                image_data['y_test']
         image_data.close()
-        return X, y
+        return self.X_train, self.X_test, self.y_train, self.y_test
