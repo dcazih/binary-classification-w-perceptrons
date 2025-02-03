0481022679a607214368488e8aa141e32246130e
Dmitri Azih <dmitriazih@gmail.com>
2025-02-02 17:38:03 -0700

    update

--- src/Main.py
+++ src/Main.py
@@ -7,14 +7,23 @@ handler = BinImgProcessor(image_data_path)
 
 if handler.processed_data_path.exists():
     print("Loading previous data ... ")
-    X, y = handler.load()
-    print("Done!\n")
+    X, y, X_test, y_test = handler.load()
+    print("Done!")
 else:
     print("Processing image data ... ")
     handler.process(image_data_path)
     print("Loading data ... ")
-    X, y = handler.load()
-    print("Done!\n")
+    X, y, X_test, y_test = handler.load()
+    print("Done!")
+
+
+perceptron = Perceptron()
+perceptron.train(X, y)
+print("Trained!")
+
+
+
+
 
 
 
