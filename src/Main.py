0224082cb48ee79d3a6deef480c808457ee728de
Dmitri Azih <dmitriazih@gmail.com>
2025-02-05 07:34:14 +0000

    added permutations each epoch

--- src/Main.py
+++ src/Main.py
@@ -1,19 +1,23 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from data_processing.BinClassifProcessor import BinClassifProcessor
 from data_processing.bin_classif_processing_utils import test_model
 from src.Perceptron import Perceptron
 from matplotlib import pyplot as plt
 
-data_path = r"C:\Users\azihd\OneDrive\Documents\binary-classification-w-perceptrons\data\url_spam_classification.csv"
+data_path = r"/workspaces/binary-classification-w-perceptrons/data/url_spam_classification.csv"
 handler = BinClassifProcessor(data_path)
 
 # Get data
-if handler.processed_data_path.exists():
+if False:#handler.processed_data_path.exists():
     print("Loading previous data ... ")
     X, y, X_test, y_test = handler.load()
     print("Done!")
 else:
     print("Processing data ... ")
-    handler.process(4000)  # param: max_features my system can handle
+    handler.process(14000)  # param: max_features my system can handle
     print("Loading data ... ")
     X, y, X_test, y_test = handler.load()
     print("Done!")
@@ -27,6 +31,6 @@ print("Done!")
 plt.plot(range(1, len(trained_model.errors_) + 1), trained_model.errors_, marker='o')
 plt.xlabel('Epochs')
 plt.ylabel('Number of updates')
-plt.show()
+#plt.show()
 
 test_model(trained_model, X_test, y_test)
