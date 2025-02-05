a7759574d6e9c941159f6f2121c8cedc332356f2
Dmitri Azih <dmitriazih@gmail.com>
2025-02-04 19:00:27 -0700

    adding processing parameters

--- src/data_processing/BinClassifProcessor.py
+++ src/data_processing/BinClassifProcessor.py
@@ -1,17 +1,20 @@
-from data_processing.bin_classif_processing_utils import preprocess
+from data_processing.bin_classif_processing_utils import preprocess, image_preprocess
 from sklearn.model_selection import train_test_split
 from pathlib import Path
 import numpy as np
 
-class BinImgProcessor:
+class BinClassifProcessor:
 
     def __init__(self, dir):
         self.dir = dir
         self.processed_data_path = Path(__file__).parent / "processed_data_dir" / "processed_data.npz"
 
-    def process(self):
-        # Obtain image data
-        self.X, self.y = preprocess(self.dir)
+    def process(self, max_features, standardize=False, images=False):
+        # Obtain data
+        if images:
+            self.X, self.y = image_preprocess(self.dir)
+        else:
+            self.X, self.y = preprocess(self.dir, "url", "is_spam", max_features, standardize)
 
         # Split the data into training and testing (90% for training, 10% for testing)
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.1,
