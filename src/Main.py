69f17729d4e2a1be61c882fcfdf377653ee8b327
Dmitri Azih <dmitriazih@gmail.com>
2025-02-06 17:03:21 +0000

    update

--- src/Main.py
+++ src/Main.py
@@ -1,10 +1,11 @@
-import sys
+import sys 
 import os
+import joblib
 import numpy as np
 from pathlib import Path
 from data_processing.BinClassifProcessor import BinClassifProcessor
 from data_processing.bin_classif_processing_utils import test_model, process_url, get_valid_url
-from src.Perceptron import Perceptron
+from Perceptron import Perceptron
 from sklearn.feature_extraction.text import CountVectorizer
 from matplotlib import pyplot as plt
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -13,9 +14,9 @@ data_path = Path(__file__).parent / r"data\url_spam_classification.csv"
 handler = BinClassifProcessor(data_path)
 
 # Get data
-getData = False
+getData = True
 if getData:
-    if handler.processed_data_path.exists():
+    if False: #handler.processed_data_path.exists():
         print("Loading previous data ... ")
         X, y, X_test, y_test = handler.load()
         print("Done!")
@@ -26,21 +27,34 @@ if getData:
         X, y, X_test, y_test = handler.load()
         print("Done!")
 
-# Start data learning
-isTrained = True
+# Start training (False) or testing (True)
+isTrained = False
 if not isTrained:
+    # Train
     perceptron = Perceptron()
     trained_model = perceptron.train(X, y)
     trained_model.save_weights("trained_model_data.npz")
+    
+    # Plot errors
+    plt.plot(range(1, len(trained_model.errors_) + 1), trained_model.errors_, marker='o')
+    plt.xlabel('Epochs')
+    plt.ylabel('Number of updates')
+    plt.savefig("plot.pdf")  # Save as PDF
+    # plt.show()
+
     print("Done!")
 else:
+    # Load a perceptron with trained weights and bias
     data = np.load(Path(__file__).parent / r"trained_model_data.npz")
     trained_model = Perceptron(data["weights"], data["bias"])
     print(f"Weights: {trained_model.w_}\n Bias: {trained_model.b_}")
 
     #test_model(trained_model, X_test, y_test)
-    vectorizer = CountVectorizer(max_features=10000)
 
+    # Load vectorizer used in training
+    vectorizer = joblib.load("vectorizer.pkl")
+
+    # Prompt user to check a url for spam
     while True:
         url = get_valid_url()
         X = process_url(url, vectorizer)
